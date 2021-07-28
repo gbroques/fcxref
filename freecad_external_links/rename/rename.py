7a20fb97daf81ebb96088478265650d9039e395e
G Roques <groques360@gmail.com>
2021-07-27 20:29:02 -0500

    Updated docstring for rename function

--- freecad_external_links/rename/rename.py
+++ freecad_external_links/rename/rename.py
@@ -7,31 +7,8 @@ from ..find import Property
 def rename(from_property: Property,
            to_property: Property) -> Dict[str, Element]:
     """
-    TODO: 1) Find from document
-             If not label (not surrounded by << >>),
-               Find file named 'XXX.FCStd'.
-             Else
-               Go through every document looking for the one wit the label
-
-          2) Then find object with name or label.
-
-                <Object name="Spreadsheet">
-                    <Properties Count="7" TransientCount="0">
-                    <Property name="Label" type="App::PropertyString" status="134217728">
-                        <String value="Spreadsheet"/>
-                    </Property>
-
-          3) Then find cell with alias.
-
-                <Property name="cells" type="Spreadsheet::PropertySheet" status="67108864">
-                    <Cells Count="2" xlink="1">
-                        <XLinks count="0">
-                        </XLinks>
-                        <Cell address="A1" content="Test" />
-                        <Cell address="B1" content="5" alias="Test" />
-                    </Cells>
-                </Property>
-
-          4) Output new XML depending upon to_reference (change alias, spreadsheet name or label).
+    TODO: Person is responsible for manually renaming the property being referenced to,
+          in the owner document.
+          This script only changes all the cross-document referencens.
     """
     pass
