250e110e2a1fbcb170326a8fa8966b3ce3f70788
G Roques <groques360@gmail.com>
2021-07-30 08:01:04 -0500

    Reformat is_label function return

--- fcxref/rename/is_label.py
+++ fcxref/rename/is_label.py
@@ -1,2 +1,5 @@
 def is_label(object_name: str) -> bool:
-    return object_name.startswith('<<') and object_name.endswith('>>')
+    return (
+        object_name.startswith('<<') and
+        object_name.endswith('>>')
+    )
