65f044fb44099275a082148df76af7c8b94532cc
G Roques <groques360@gmail.com>
2021-08-01 09:11:47 -0500

    Update rename test for owner document

--- tests/rename_test.py
+++ tests/rename_test.py
@@ -4,13 +4,14 @@ from typing import Dict
 from xml.etree import ElementTree
 from xml.etree.ElementTree import Element
 
-from fcxref.find import Property
 from fcxref.rename import make_rename
 
 
 def find_root_by_document_path(base_path: str, document_pattern: str = '*') -> Dict[str, Element]:
+    document = 'MasterDocument.xml' if document_pattern == 'Master' else 'Document.xml'
+    filepath = 'Master.FCStd' if document_pattern == 'Master' else 'Test.FCStd'
     return {
-        'Test.FCStd': load_root('Document.xml')
+        filepath: load_root(document)
     }
 
 
@@ -25,15 +26,25 @@ class RenameTest(unittest.TestCase):
 
     def test_rename(self):
         rename = make_rename(find_root_by_document_path)
+
         root_by_document_path = rename('base_path',
                                        'Master',
                                        'Spreadsheet',
                                        ('Value', 'RenamedValue'))
+
+        self.assertEqual(len(root_by_document_path.items()), 2)
+
         self.assertIn('Test.FCStd', root_by_document_path)
-        root = root_by_document_path['Test.FCStd']
-        expected_root = load_root('RenamedDocument.xml')
-        self.assertEqual(ElementTree.tostring(root),
-                         ElementTree.tostring(expected_root))
+        document_root = root_by_document_path['Test.FCStd']
+        expected_document_root = load_root('RenamedDocument.xml')
+        self.assertEqual(ElementTree.tostring(document_root),
+                         ElementTree.tostring(expected_document_root))
+
+        self.assertIn('Master.FCStd', root_by_document_path)
+        master_root = root_by_document_path['Master.FCStd']
+        expected_master_root = load_root('RenamedMasterDocument.xml')
+        self.assertEqual(ElementTree.tostring(master_root),
+                         ElementTree.tostring(expected_master_root))
 
 
 if __name__ == '__main__':
