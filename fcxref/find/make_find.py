999d7b9797181b18b950f7d73f3ce591c9f8c8d6
G Roques <groques360@gmail.com>
2024-12-24 13:09:28 -0600

    wip

--- fcxref/find/make_find.py
+++ fcxref/find/make_find.py
@@ -1,4 +1,5 @@
 import re
+from pathlib import Path
 from typing import Callable, Dict, List
 from xml.etree.ElementTree import Element
 
@@ -14,15 +15,29 @@ def make_find(find_root_by_document_path: Callable[[str], Dict[str, Element]]):
         references = []
         root_by_document_path = find_root_by_document_path(base_path)
         for document_path, root in root_by_document_path.items():
-            query_pattern = re.compile(query.to_regex())
-            references_in_document = find_references_in_root(
-                document_path, root, query_pattern)
-            references.extend(references_in_document)
-            if len(references_in_document) and query.property_name:
-                text_pattern = re.compile(r'\b{}(?<!{})\b'.format(
-                    query.property_name, query.to_regex()))
-                text_references_in_document = find_references_in_root(
-                    document_path, root, text_pattern)
+            document_name = Path(document_path).stem
+            # Look for relative references
+            if document_name == query.document:
+                print(f'Looking for {query._to_string()} in local document ' + query.document)
+                relative_property_pattern = re.compile(query.property_name)
+                text_references_in_document = find_references_in_root(document_path, root, relative_property_pattern)
+                print(text_references_in_document)
                 references.extend(text_references_in_document)
+
+
+                # relative_property_pattern = re.compile(f'{query.object_name}\\.{query.property_name}')
+                # text_references_in_document = find_references_in_root(document_path, root, relative_property_pattern)
+                # references.extend(text_references_in_document)
+            else:
+                query_pattern = re.compile(query.to_regex())
+                references_in_document = find_references_in_root(
+                    document_path, root, query_pattern)
+                references.extend(references_in_document)
+                if len(references_in_document) and query.property_name:
+                    text_pattern = re.compile(r'\b{}(?<!{})\b'.format(
+                        query.property_name, query.to_regex()))
+                    text_references_in_document = find_references_in_root(
+                        document_path, root, text_pattern)
+                    references.extend(text_references_in_document)
         return references
     return find
--- tests/find_test.py
+++ tests/find_test.py
@@ -8,11 +8,15 @@ from fcxref.find import Query, Reference, make_find
 
 
 def find_root_by_document_path(base_path: str) -> Dict[str, Element]:
-    document_xml_path = Path(__file__).parent.joinpath('ExampleDocument.xml')
-    with open(document_xml_path) as f:
-        document_xml = f.read()
+    document_paths = ['MainDocument.xml', 'ExampleDocument.xml']
+    xml_by_path = {}
+    for path in document_paths:
+        document_xml_path = Path(__file__).parent.joinpath(path)
+        with open(document_xml_path) as f:
+            xml_by_path[path] = f.read()
     return {
-        'ExampleDocument.FCStd': ElementTree.fromstring(document_xml)
+        'MainDocument.FCStd': ElementTree.fromstring(xml_by_path['MainDocument.xml']),
+        'ExampleDocument.FCStd': ElementTree.fromstring(xml_by_path['ExampleDocument.xml'])
     }
 
 
@@ -23,7 +27,7 @@ class FindTest(unittest.TestCase):
         references = find('base_path',
                           Query('MainDocument', 'Spreadsheet', 'Value'))
 
-        self.assertEqual(len(references), 4)
+        self.assertEqual(len(references), 6)
 
         xpath0 = "ObjectData/Object[@name='Spreadsheet']/Properties/Property[@name='cells']/Cells/Cell[@address='B1']"
         self.assertEqual(references[0],
@@ -87,6 +91,18 @@ class FindTest(unittest.TestCase):
                                    'MainDocument#Spreadsheet',
                                    '=MainDocument#Spreadsheet.Value',
                                    xpath0))
+
+        xpath1 = "ObjectData/Object[@name='Box']/Properties/Property[@name='ExpressionEngine']/ExpressionEngine/Expression[@path='Length']"
+        self.assertEqual(references[1],
+                         Reference('ExampleDocument.FCStd',
+                                   'Box',
+                                   'ExpressionEngine',
+                                   'expression',
+                                   'Length',
+                                   'MainDocument#Spreadsheet',
+                                   'MainDocument#Spreadsheet.Value',
+                                   xpath1))
+    
 
 
 if __name__ == '__main__':
